b8fb3683ff175a0f1d8de6483b481042ee112417
Gandharva Hardikar <gandharva.hardikar@gmail.com>
2025-07-13 11:39:56 +0530

    Lec No 2

---
+++ assignment2sum.js
@@ -0,0 +1,15 @@
+//Write a function sum that finds the sum of two numbers. 
+//Side quest - Try passing in a string instead of a number and see what happens?
+
+function sum(a,b)
+{   
+    let totalSum = a + b;
+    return totalSum;
+}
+    let ans = sum(20, 30);
+    let ans2 = sum(56, 69);
+
+    console.log(ans);
+    console.log(ans2);  
+    
+
