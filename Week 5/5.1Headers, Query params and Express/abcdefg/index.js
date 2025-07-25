5d46008b8921c47b042e605294f7facc1f63c0bb
Gandharva Hardikar <gandharva.hardikar@gmail.com>
2025-07-25 21:31:10 +0530

    Week 5 Day 1 continued

---
+++ Week 5/5.1Headers, Query params and Express/abcdefg/index.js
@@ -0,0 +1,18 @@
+//creating a backend server (http)
+
+const express = require('express');
+
+const app = express();
+
+app.get("/sum", function(req, res)
+{
+    
+})
+
+app.get("/multiply", function(req, res)
+{
+
+})
+ 
+
+app.listen(3000);
